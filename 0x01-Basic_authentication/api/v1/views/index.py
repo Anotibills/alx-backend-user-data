6d97b27cdd7571c592b7810f72cc3de439200e25
Anotibills <anotibills@gmail.com>
2024-02-14 04:35:53 +0100

    This is the API files being added

---
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+This is the module on the index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    '''
+    This returns the status of the API
+    '''
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    '''
+    This returns the number of each objects
+    '''
+    from models.user import User
+    stats = {"users": User.count()}
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    '''
+    This returns the error handler for 401 will be executed
+    '''
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    '''
+    This returns the error handler for 401 will be executed
+    '''
+    abort(403)
