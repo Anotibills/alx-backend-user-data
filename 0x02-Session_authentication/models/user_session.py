13bce275cd5e0385f3640fa8041496316de72153
Anotibills <anotibills@gmail.com>
2024-02-16 05:27:19 +0100

    User session added

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This is the user session module
+"""
+from models.base import Base
+
+
+class UserSession(Base):
+    '''
+    This is ths class for User Session
+    '''
+
+    def __init__(self, *args: list, **kwargs: dict):
+        '''
+        This initializes a User instance
+        '''
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
